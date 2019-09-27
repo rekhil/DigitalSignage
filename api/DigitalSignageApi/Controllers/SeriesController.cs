2a4bcab8dc66946fcf00bd8f9dfb197d50fab210
solomon <solomonsahayaraj911@gmail.com>
2019-09-27 17:53:49 +0530

    Adding API Endpoints

---
+++ api/DigitalSignageApi/Controllers/SeriesController.cs
@@ -0,0 +1,43 @@
+﻿using DigitalSignageApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DigitalSignageApi.Controllers
+{
+    public class SeriesController : ApiController
+    {
+        // GET: api/Series
+        public IEnumerable<Series> GetAllSeries()
+        {
+            return new List<Series>();
+        }
+
+        // GET: api/Series/5
+        public Series GetSeries(int seriesId)
+        {
+            return new Series();
+        }
+
+        // POST: api/Series
+        public void Post([FromBody]Series series)
+        {
+
+        }
+
+        // PUT: api/Series/5
+        public void Put(int id, [FromBody]Series series)
+        {
+
+        }
+
+        // DELETE: api/Series/5
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
---
+++ api/DigitalSignageApi/Controllers/SlideController.cs
@@ -0,0 +1,43 @@
+﻿using DigitalSignageApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DigitalSignageApi.Controllers
+{
+    public class SlideController : ApiController
+    {
+        // GET: api/Slide
+        public IEnumerable<Slide> GetAllSlides()
+        {
+            return new List<Slide>();
+        }
+
+        // GET: api/Slide/5
+        public Slide Get(int slideId)
+        {
+            return new Slide();
+        }
+
+        // POST: api/Slide
+        public void Post([FromBody]Slide slide)
+        {
+
+        }
+
+        // PUT: api/Slide/5
+        public void Put(int id, [FromBody]Slide slide)
+        {
+
+        }
+
+        // DELETE: api/Slide/5
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
