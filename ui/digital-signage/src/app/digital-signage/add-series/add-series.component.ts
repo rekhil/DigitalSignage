23fe9f7d9941e748251f87693d5f9c99d77b02d4
Rekhil Das <>
2019-09-27 22:28:11 +0530

    slide component

--- ui/digital-signage/src/app/digital-signage/add-series/add-series.component.ts
+++ ui/digital-signage/src/app/digital-signage/add-series/add-series.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from '../shared/service';
-import { Series } from '../shared/model';
+import { Series, Slide } from '../shared/model';
 
 @Component({
   selector: 'app-add-series',
@@ -10,9 +10,18 @@ import { Series } from '../shared/model';
 export class AddSeriesComponent {
 
   private series: Series;
+  private slideList: Slide[];
 
   constructor(public dataService: DataService) {
     this.series = new Series();
+
+    this.slideList = [
+      { slideId: 1, duration: 10, templateId: 1, filePath: '../assets/files/dominos.jpg' },
+      { slideId: 2, duration: 15, templateId: 1, filePath: '../assets/files/mac.jpg' },
+      { slideId: 3, duration: 5, templateId: 1, filePath: '../assets/files/burger_king.gif' },
+      { slideId: 4, duration: 15, templateId: 1, filePath: '../assets/files/kfc.jpg' },
+      { slideId: 5, duration: 8, templateId: 1, filePath: '../assets/files/chicking.jpg' }
+    ];
   }
 
   ngOnInit() {
