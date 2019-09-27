2a1668dfc7364a77ff898d5e42722732fb49c606
solomon <solomonsahayaraj911@gmail.com>
2019-09-27 19:09:56 +0530

    added repository class

---
+++ api/DigitalSignageApi/Repository/DigitalSignageRepository.cs
@@ -0,0 +1,40 @@
+﻿using DigitalSignageApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DigitalSignageApi.Repository
+{
+    public class DigitalSignageRepository
+    {
+        private readonly string connectionString = "Server=INPCD0228\\SQLEXPRESS;DataBase=DeltaxMovies;Integrated Security = SSPI;MultipleActiveResultSets=true";
+
+        public List<Slide> GetAllSlides()
+        {
+            List<Slide> Slides = new List<Slide>();
+            using (var conn = new SqlConnection(connectionString))
+            using (var command = new SqlCommand("pr_GetAllSlides", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            })
+            {
+                conn.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var item = new Slide();
+                    item.Duration = (Int32)reader["Duration"];
+                    item.SlideId = (Int32)reader["SlideId"];
+                    item.TemplateId = (Int32)reader["TemplateId"];
+                    Slides.Add(item);
+                }
+                reader.Close();
+            }
+
+            return Slides;
+        }
+    }
+}
