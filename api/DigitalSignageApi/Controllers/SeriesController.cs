c9f73a069b8ed0c0aca92de234eb097e8a8a120b
solomon <solomonsahayaraj911@gmail.com>
2019-10-05 23:40:34 +0530

    removed unused usings

--- api/DigitalSignageApi/Controllers/SeriesController.cs
+++ api/DigitalSignageApi/Controllers/SeriesController.cs
@@ -2,11 +2,7 @@
 using DigitalSignageApi.Repository;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Cors;
--- api/DigitalSignageApi/Models/Location.cs
+++ api/DigitalSignageApi/Models/Location.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace DigitalSignageApi.Models
+﻿namespace DigitalSignageApi.Models
 {
     public class Location
     {
